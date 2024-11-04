4db3c57ae04ad626dd768cc09699972a555501dd
Ilia Kamilov <iliakmlv@gmail.com>
2024-11-04 12:33:31 +0200

    init: git install: core dependencies

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @file src\index.ts
+ * @description Main entry point for the api project
+ * @author Ilia Kamilov <iliakmlv@gmail.com> (https://github.com/iliakamilov)
+ * @date 04/11/2024
+ * @version 0.0.0
+ */
