a236402aec60271ad50589c3c46b6a97186e7f23
Ilia Kamilov <iliakmlv@gmail.com>
2024-11-08 19:33:18 +0200

    add: User interface

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -1,36 +1,9 @@
-import mongoose from "mongoose";
-
-export interface User {
-  username: string;
-  password: string;
-  firstname: string;
-  lastname: string;
+interface User {
+  readonly id: string;
+  readonly username: string;
+  readonly password: string;
+  readonly firstname: string;
+  readonly lastname: string;
 }
 
-const userSchema: mongoose.Schema<User> = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 6,
-    },
-    firstname: {
-      type: String,
-      required: true,
-    },
-    lastname: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
-
-const UserModel = mongoose.model<User>("User", userSchema);
-
-export default UserModel;
+export default User;
